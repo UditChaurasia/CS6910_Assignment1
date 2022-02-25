735b644986f4467516a9ec37b1cbb825b5d78a98
UditChaurasia <100060926+UditChaurasia@users.noreply.github.com>
2022-02-25 19:58:42 +0530

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -1,16 +1,25 @@
 from matplotlib import pyplot
 from keras.datasets import fashion_mnist
-# load dataset
-(trainX, trainY), (testX, testy) = fashion_mnist.load_data()
-# summarize loaded dataset
-count = 0
-temp = [11]
-for i in range(0,60000):
-  if (count == 10):
-    break
-  else:
-    if (trainY[i] not in temp):
-      temp.append(trainY[i])
-      print("Image corresponding to the label : ", trainY[i])
-      pyplot.imshow(trainX[i], cmap=pyplot.get_cmap('gray'))
-      pyplot.show()
+def img_plot():
+  # load dataset
+  (trainX, trainY), (testX, testy) = fashion_mnist.load_data()
+  # summarize loaded dataset
+  count = 0
+  temp = [11]
+  for i in range(0,60000):
+    if (count == 10):
+      break
+    else:
+      if (trainY[i] not in temp):
+        temp.append(trainY[i])
+        image = wandb.Image(trainX[i], caption="Label Image")
+        wandb.log({"Label": image})
+      
+
+      
+!pip install wandb
+import wandb
+wandb.login
+
+wandb.init(project = 'Assignment1', entity = 'uditchaurasia', reinit = True)
+img_plot()
